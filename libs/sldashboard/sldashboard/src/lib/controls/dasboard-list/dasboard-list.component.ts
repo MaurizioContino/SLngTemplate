3a119e8179796f88caedb87f9402bf6e6de63edb
Maurizio Contino <m.contino@pipegrep.it>
2023-01-24 18:00:56 +0100

    Sistemato componenti dinamici

--- libs/sldashboard/sldashboard/src/lib/controls/dasboard-list/dasboard-list.component.ts
+++ libs/sldashboard/sldashboard/src/lib/controls/dasboard-list/dasboard-list.component.ts
@@ -1,34 +1,24 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component,  EventEmitter, Input, Output, QueryList, TemplateRef } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter,Output } from '@angular/core';
+import { WidgetStatus } from '../../models/WidgetStatus';
+import { DashboardConfigService } from '../../services/dashboard.service';
 
 @Component({
-  selector: 'sl-dashboard-item-list',
-  templateUrl: './dasboard-list.component.html',
-  styleUrls: ['./dasboard-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'sl-dashboard-item-list',
+    templateUrl: './dasboard-list.component.html',
+    styleUrls: ['./dasboard-list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DasboardListComponent {
+    // implements OnInit, OnDestroy {
 
-  private _contents: QueryList<TemplateRef<any>> | undefined;
-
-   @Input()
-  
-  public get contents(): QueryList<TemplateRef<any>> | undefined {
-    return this._contents;
-  }
-  public set contents(value: QueryList<TemplateRef<any>> | undefined) {
-    this._contents = value;
-    if (value) {
-      const dbg = value.toArray()
-      console.log(dbg)
-      console.log(dbg[0])
+    public  selectStatus: WidgetStatus = WidgetStatus.select
+    @Output() Selected = new EventEmitter<number>();
+    
+    GetElemetsAsIDashboard(): any[] {
+        return this.dashserv.Widgets.map((v: any)=>{
+            return v;
+        })
     }
-  }
-   @Output() Selected = new EventEmitter<number>()
-  constructor(private cdr: ChangeDetectorRef) {
-  }
 
-  getContext() {
-    return {status: 'select'};
-  }
-
+    constructor(private cdr: ChangeDetectorRef, public dashserv: DashboardConfigService) {}
 }
